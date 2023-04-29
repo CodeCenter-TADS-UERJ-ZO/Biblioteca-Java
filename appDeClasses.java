6e20096c75ccdad102768dc8bea90135868596d8
Daniel Lourenço Affonso <86497631+Daniel-online@users.noreply.github.com>
2023-04-28 21:39:15 -0300

    Create appDeClasses.java

---
+++ appDeClasses.java
@@ -0,0 +1,15 @@
+public class appDeClasses{
+//atributos
+        boolean disponivel;
+        double preco;
+        char categoria;
+        String nomeProduto;
+        int quatidadeDisponivel;
+    public static void main(String[] args) {
+                
+    
+
+    }
+
+
+}
