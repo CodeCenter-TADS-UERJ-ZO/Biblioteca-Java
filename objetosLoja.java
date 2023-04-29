85fbec901ace316c91a0aba822c33d4ca368d7a3
Daniel Lourenço Affonso <86497631+Daniel-online@users.noreply.github.com>
2023-04-28 21:38:40 -0300

    Create objetosLoja.java

---
+++ objetosLoja.java
@@ -0,0 +1,23 @@
+public class objetosLoja{
+   public void setPreco{
+        
+    Scanner preco = New Scanner(SWystem)
+
+    System.out.println("Insira o preço:");
+   }
+   public void setNomeProduto{
+        System.out.println("Insira o nome do produto:")
+   }
+   public void setCategoria{
+
+   }
+   public void  quatidadeDisponivel{
+
+   }
+   public void setDisponibilidade{
+
+   }
+
+
+
+}
